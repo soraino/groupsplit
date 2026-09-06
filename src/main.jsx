690422b9a6ca96333683c736470d9403c281a9d4
Tang Kok Foon <kf@Tangs-MacBook-Air.local>
2026-09-06 16:58:16 +0800

    setting basename for react router helped

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import { BrowserRouter } from "react-router";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/groupsplit">
       <App />
     </BrowserRouter>
   </StrictMode>
